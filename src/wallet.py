a927ca36057ee5d85bf8ef18d3f851f11be62b43
atdykema <atdykema@gmail.com>
2021-05-21 18:25:26 -0400

    wallet generate sha256 hash

--- src/wallet.py
+++ src/wallet.py
@@ -1,22 +1,36 @@
 import time
 import random
 import tools.tools
+import os
+import hashlib
 
 
 class Wallet:
     def __init__(self):
         mnemonic_words = []
-        binary_generation = []
+        total_word_digits = []
+        current_word_digits = []
+        f = open("BIP39_wordlist.txt", "r")
+        word_list = f.readlines()
+        f.close()
         for n in range(0, 256):
-            binary_generation.append(str(random.randint(0, 1)))
-            if(len(binary_generation) % 11 == 0):
+            current_word_digits.append(str(random.randint(0, 1)))
+            if len(current_word_digits) % 11 == 0:
                 #convert list to string
-                tools.tools.list_to_string(binary_generation)
+                binary_string = tools.tools.list_to_string(current_word_digits)
                 #string to decimal from binary
-                print(binary_generation)
-                print(n / 11)
-                # add one, get word from word list
-                binary_generation.clear()
+                decimal = tools.tools.binary_to_decimal(binary_string)
+                #get word from word list
+                mnemonic_words.append(word_list[decimal][0:-1])
+                #add digits to total list
+                tools.tools.list_chars_to_other_list(current_word_digits, total_word_digits)
                 #clear list
-
-
+                current_word_digits.clear()
+        check_sum = tools.tools.list_to_string(current_word_digits)
+        #get sha256 of 256 word digits
+        str_total_word_digits = tools.tools.list_to_string(total_word_digits)
+        sha256_string = hashlib.sha256(bytes(str_total_word_digits, encoding='utf-8'))
+        sha256_hex = sha256_string.hexdigest()
+        print(sha256_hex)
+        print(mnemonic_words)
+        print(check_sum)
--- tools/tools.py
+++ tools/tools.py
@@ -6,3 +6,11 @@ def list_to_string(list):
         s += digit
     return s
 
+
+def binary_to_decimal(n):
+    return int(n, 2)
+
+
+def list_chars_to_other_list(from_list, to_list):
+    for digit in from_list:
+        to_list.append(digit)
