63522b08e1a536240b2a594f06a2f584b20d5565
atdykema <atdykema@gmail.com>
2021-06-14 18:52:56 -0400

    rearranged tx class for testing

--- src/tx.py
+++ src/tx.py
@@ -4,9 +4,9 @@
 # TODO: get nonce
 
 class Tx:
-    def __init__(self, gas_limit, sending_address, receiving_address, unit_exchanged):
+    def __init__(self, sending_address, receiving_address, unit_exchanged):
         # self.nonce = function that searches blockchain for number of transactions per address by sending_address
-        self.gas_limit = gas_limit
+        # self.gas_limit = gas_limit
         self.sending_address = sending_address
         self.receiving_address = receiving_address
         self.unit_exchanged = unit_exchanged
