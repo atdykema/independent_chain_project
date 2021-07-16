20d8581e29d6d3b0c49fa61e8fc10e59fc4edfe0
Andrew Dykema <atdykema@gmail.com>
2021-07-16 16:59:54 -0400

    added spacing to merkle_hash func

--- src/mining.py
+++ src/mining.py
@@ -24,27 +24,49 @@ def find_target(difficulty):
 
 
 def get_hash_merkle_root(transactions):
+
     tx = transactions.copy()
+
     if len(tx) == 0:
+
         return []
+
     temp = []
+
     while len(tx) > 1:
+
         concat = tx[len(tx)-1] + tx[len(tx)-2]
+
         curr_hash = sha256(concat.encode('utf-8')).hexdigest()
+
         temp.append(curr_hash)
+
         del tx[len(tx) - 1]
+
         del tx[len(tx) - 1]
+
     if len(tx) == 1:
+
         tx.append(tx[0])
+
         concat = tx[0] + tx[1]
+
         curr_hash = sha256(concat.encode('utf-8')).hexdigest()
+
         temp.append(curr_hash)
+
         del tx[len(tx) - 1]
+
         del tx[len(tx) - 1]
+
     if len(temp) == 1:
+
         return temp[0]
+
     else:
+
         hash_ = get_hash_merkle_root(temp)
+        
         return hash_
 
 
